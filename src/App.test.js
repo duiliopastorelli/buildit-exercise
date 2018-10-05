bb791fd0fc945bcfccf0cd1e8c63d7cf0600ae82
Danilo Mezgec <mezgec.danilo@gmail.com>
2018-10-05 16:05:02 +0200

    feat: adds the 1st test and fixes the leak of memory issue on the async setState call after the API fetch

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {expect} from 'chai';
+import {fullWeatherAPIResponse} from './mocks/mocks'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  let localStorage = window.localStorage;
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({adapter: new Adapter()});
+
+describe('<App />', () => {
+
+  it('renders <App/> without crashing', () => {
+    beforeEach(() => {
+      fetch.resetMocks()
+    });
+
+    //Provides a mock for the fetchForecasts API call
+    fetch.mockResponseOnce(JSON.stringify(fullWeatherAPIResponse));
+
+    const wrapper = shallow(<App/>);
+    expect(wrapper).to.have.lengthOf(1);
+  });
+
 });
+
